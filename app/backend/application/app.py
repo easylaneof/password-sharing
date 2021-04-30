d693df92cd800f1075f311bcff20ed45839a81f1
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-04-30 15:14:02 +0300

    fix(backend): moved the app initialization

--- app/backend/application/app.py
+++ app/backend/application/app.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask import Blueprint
 from flask_cors import CORS
+
 main = Blueprint('main', __name__)
 
 
@@ -10,5 +11,11 @@ def create_app():
     CORS(app, supports_credentials=True)
     app.register_blueprint(main)
     with app.app_context():
-        from application.routes import general
+        from routes import general
         return app
+
+
+application = create_app()
+
+if __name__ == '__main__':
+    application.run()
