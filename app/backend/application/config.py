5d1ccbcde9cd0d747185eb8d174aae84d2850efb
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-05-01 21:13:32 +0300

    feat(backend): changed DB handler to SQLAlchemy

--- app/backend/application/config.py
+++ app/backend/application/config.py
@@ -1,10 +1,23 @@
+from os import environ, path
+from dotenv import load_dotenv
+
+basedir = (path.dirname(__file__))
+load_dotenv(path.join(basedir, '.env'))
+
+
 class Config:
     STATIC_FOLDER = 'static'
+    SQLALCHEMY_DATABASE_URI = environ.get('DATABASE_URL')
+    JWT_TOKEN_LOCATION = ('headers', 'query_string')
+    JWT_QUERY_STRING_NAME = 'token'
+    JWT_HEADER_TYPE = ''
 
 
 class DevelopmentConfig(Config):
     FLASK_ENV = 'development'
     TESTING = True
+    SQLALCHEMY_ECHO = False
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
 
 
 class ProductionConfig(Config):
