b463884c30008daecfb300af0ee2580cb4f1d9a3
fokysland <trashfokys@yandex.ru>
2021-05-05 21:17:41 +0500

    feat(frontend): added <Button />

--- app/frontend/src/components/molecules/Button/Button.component.tsx
+++ app/frontend/src/components/molecules/Button/Button.component.tsx
@@ -1,9 +1,17 @@
-import React, { useState } from 'react';
+import React from 'react';
+
+import cx from 'classnames';
+
+import { Text } from 'components/atoms/Text';
 
 import { ButtonProps } from './Button.interface';
 
 import s from './Button.module.scss';
 
-export const Button = ({ text }: ButtonProps): JSX.Element => {
-  return <button className={s.container}>{text}</button>;
+export const Button = ({ text, onClick, className, disabled }: ButtonProps): JSX.Element => {
+  return (
+    <button className={cx(s.container, className)} type="button" onClick={onClick} disabled={disabled}>
+      <Text text={text} color="white" />
+    </button>
+  );
 };
