5b1f31922903d2111eb4ad18d109fdd6774096f4
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-05-05 17:30:12 +0300

    fix(backend): added error_handling.py and imported it

--- app/backend/application/app.py
+++ app/backend/application/app.py
@@ -2,8 +2,6 @@ from flask import Flask
 from flask import Blueprint
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
-from sqlalchemy import event
-from sqlalchemy.engine import Engine
 
 main = Blueprint('main', __name__)
 db = SQLAlchemy()
@@ -13,20 +11,13 @@ def create_app():
     app = Flask(__name__)
     app.config.from_object('config.DevelopmentConfig')
     CORS(app, supports_credentials=True)
-  #  db.init_app(app)
+    db.init_app(app)
     app.register_blueprint(main)
     with app.app_context():
-        from routes import general
+        from routes import general, error_handling
         import dbmodels
-#        db.create_all()
+        db.create_all()
         return app
-
-
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
 
 
 application = create_app()
