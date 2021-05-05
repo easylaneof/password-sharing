33d7271060d9b16b7f858832a7d1415f512d423c
fokysland <trashfokys@yandex.ru>
2021-05-05 21:14:55 +0500

    feat(frontend): <TextInput />

---
+++ app/frontend/src/components/molecules/TextInput/TextInput.component.tsx
@@ -0,0 +1,33 @@
+import React, { forwardRef } from 'react';
+
+import cx from 'classnames';
+
+import { TextInputProps } from './TextInput.interface';
+
+import s from './TextInput.module.scss';
+
+export const TextInput = forwardRef<HTMLInputElement, TextInputProps>(
+  ({ setValue, value, className, readonly, name, onChange, placeholder }, ref): JSX.Element => {
+    const handleChange: React.ChangeEventHandler<HTMLInputElement> = (e) => {
+      if (setValue) {
+        setValue(e.target.value);
+      }
+
+      if (onChange) {
+        onChange(e);
+      }
+    };
+
+    return (
+      <input
+        name={name}
+        readOnly={readonly}
+        ref={ref}
+        className={cx(s.container, className)}
+        value={value}
+        onChange={handleChange}
+        placeholder={placeholder}
+      />
+    );
+  }
+);
