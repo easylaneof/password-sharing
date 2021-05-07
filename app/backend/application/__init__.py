cb70978e284b7c0952aa0fc3a992be137dc8066b
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-05-07 16:49:37 +0300

    fix(backend): app init fix

--- app/backend/application/__init__.py
+++ app/backend/application/__init__.py
@@ -3,11 +3,16 @@ from flask import Blueprint
 from flask_cors import CORS
 from flask_sqlalchemy import SQLAlchemy
 
-app = Flask(__name__)
-app.config.from_object("application.config.DevelopmentConfig")
-db = SQLAlchemy(app)
-CORS(app, supports_credentials=True)
-with app.app_context():
-    from application.routes import general, error_handling
-    from application import dbmodels
-    db.create_all()
+db = SQLAlchemy()
+
+
+def create_app():
+    app = Flask(__name__)
+    app.config.from_object("application.config.DevelopmentConfig")
+    CORS(app, supports_credentials=True)
+    db.init_app(app)
+    with app.app_context():
+        from application.routes import general, error_handling
+        from application import dbmodels
+        db.create_all()
+    return app
--- app/backend/run.py
+++ app/backend/run.py
@@ -1,4 +1,5 @@
-from application import app
+from application import create_app
 
 if __name__ == "__main__":
-    app.run(debug=True)
+    app = create_app()
+    app.run()
