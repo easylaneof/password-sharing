f42ca3a27c0bd9e17f21b114888cd128a675a579
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-05-08 19:21:18 +0300

    feat(backend): fix utils.py for proper validation

--- app/backend/application/utils.py
+++ app/backend/application/utils.py
@@ -19,13 +19,15 @@ def validator(request, keys):
 def convert_str_to_non_negative_int(string: str, error_message: str):
     try:
         converted = int(string)
+        if converted < 0:
+            raise ValueError
         return converted
     except ValueError:
         raise werkzeug.exceptions.BadRequest(error_message)
 
 
 def convert_str_to_positive_int(string: str, error_message: str):
-    integer = convert_str_to_non_negative_int(string, error_message)
-    if integer == 0:
+    converted = convert_str_to_non_negative_int(string, error_message)
+    if converted == 0:
         raise werkzeug.exceptions.BadRequest(error_message)
-    return integer
+    return converted
