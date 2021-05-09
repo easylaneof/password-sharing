536a07c005ff230b1ccaf2d331764ae88b34ee32
Mikhail <55994400+Alan-Shan@users.noreply.github.com>
2021-05-09 15:40:47 +0300

    feat(backend): link expiration time check

--- app/backend/application/database.py
+++ app/backend/application/database.py
@@ -1,6 +1,5 @@
-import datetime
-
 import werkzeug.exceptions
+from datetime import datetime, timedelta
 from application.dbmodels import Sessions
 from application import db
 
@@ -13,6 +12,9 @@ def create_record(session: Sessions):
 def get_validate_record(record_id: str):
     db_record = db.session.query(Sessions).filter_by(id=record_id).first()
     if db_record is None:
+        raise werkzeug.exceptions.BadRequest("Incorrect ID")
+    if db_record.expiry and db_record.expiry < datetime.utcnow():
+        delete_record(record_id)
         raise werkzeug.exceptions.BadRequest("Incorrect ID")
     return db_record
 
@@ -27,9 +29,10 @@ def set_record_uses_and_expiry(record_id: str, max_uses: int, expiry_hours: int,
     if record is None:
         raise werkzeug.exceptions.BadRequest("Incorrect ID")
     record.max_uses = max_uses
-    td = datetime.timedelta(hours=expiry_hours,
-                            minutes=expiry_minutes)
-    record.expiry = datetime.datetime.utcnow() + td
+    if not (expiry_hours == 0 and expiry_minutes == 0):
+        td = timedelta(hours=expiry_hours,
+                       minutes=expiry_minutes)
+        record.expiry = datetime.utcnow() + td
     db.session.commit()
 
 
